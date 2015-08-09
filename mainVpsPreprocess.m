0267c51f1bc519ccc06d1c253b1b775b0a6844e7
Shubham Tulsiani <shubhtuls@gmail.com>
2015-08-09 17:37:19 -0400

    minor path modification

--- mainVpsPreprocess.m
+++ mainVpsPreprocess.m
@@ -25,11 +25,12 @@ rcnnMultibinnedJointTrainValTestCreate([24 16 8 4]); % generates window file for
 % PATH_TO_PROTOTXT_DIR/vggJointVps/trainTest.prototxt and PATH_TO_PROTOTXT_DIR/vggAzimuthVps/trainTest.prototxt
 % to refer to the Train/Val files created by above functions.
 
-% we train two networks here - one for 
-% the shell scripts look as follows
+% we train two networks here - one for predicting all the euler angles, other for various bin sizes of azimuth as required by AVP evaluation
 
+% the shell scripts look as follows
 % ./build/tools/caffe.bin train -solver PATH_TO_PROTOTXT_DIR/vggJointVps/solver.prototxt -weights PATH_TO_PRETRAINED_VGG_CAFFEMODEL
 % ./build/tools/caffe.bin train -solver PATH_TO_PROTOTXT_DIR/vggAzimuthVps/solver.prototxt -weights PATH_TO_PRETRAINED_VGG_CAFFEMODEL
+% after training the models, save the final snapshot in SNAPSHOT_DIR/finalSnapshots/[vggJointVps,vggAzimuthVps].caffemodel/
 
 %% Compute features
 generatePoseFeatures('vggJointVps','vggJointVps',224,params.classInds,1); % needed for evaluation
